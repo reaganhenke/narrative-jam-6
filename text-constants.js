cf43f1ef653de6095e495daed655cbc6a7c25d07
LaChapeliere <emmamarion.barme@orange.fr>
2022-03-21 15:07:07 +0100

    Adding French version for all text

--- text-constants.js
+++ text-constants.js
@@ -18,7 +18,7 @@ const textConstantsEnglish = {
       It's a tricky balance to find: people want drama, drama, drama, until suddenly
       they think you sound fake. And sometimes, the ghosts' requests are just so boring…
       So if Joss plays fast and loose with her interpretation, no one has to know, right?
-      Especially not that sucker from Spectral Self-Help who's also sniffing around
+      Especially not that sucker from Spectral Self-Help who's always sniffing around
       trying to find something to bring Ghost Whisperer down...
     </p>
     <P>
@@ -37,35 +37,36 @@ const textConstantsEnglish = {
 const textConstantsFrench = {
   aboutText: `
     <p>
-      Ghost Whisperer is brought to you by the marvelous Joss!
-      FalseTrout (sound), IfThenCreate (tech), LaChapeliere (writing) and NightSparrow
-      (visual) make it work behind the scenes.
+      Ghost Whisperer vous est présenté par la merveilleuse Joss!
+      FalseTrout (son), IfThenCreate (tech), LaChapeliere (écriture) and NightSparrow
+      (graphismes) oeuvrent dans les coulisses.
     </p>
-    <p>Equipped with her trusted ghost interpreter, Joss travels across the country
-      to hear what ghosts have on their minds and assist in settling their unfinished business.
-      So if you have a ghost you'd like helped into the other side, give us a call!</p>
+    <p>Munie de son fidèle interpréteur de fantômes, Joss parcoure le pays pour écouter ce que les
+      fantômes ont sur le coeur et les aider à résoudre ce qui les retient sur Terre. Alors si vous
+      avez un fantôme que vous voulez aider à passer dans l'au-délà, appelez-nous !</p>
     <p>
-      These are exciting times for Joss, as she's just been contacted by a big company
-      who wants to sponsor her show! Over the next month, she needs to rack up as many
-      viewers as possible in order to impress them. She'll have to up her game, because
-      we all know that the BooTube crowd isn't a forgiving one...
+      C'est une période excitante pour Joss, car elle vient d'être contactée par une grosse boîte
+      qui veut sponsoriser son émission ! Dans le prochain mois elle va devoir accumuler le plus
+      d'auditeurs possible pour les impressioner. Elle va devoir assurer, parce qu'on sait tous que
+      l'audience de BooTube est loin d'être indulgente...
     </p>
     <p>
-      It's a tricky balance to find: people want drama, drama, drama, until suddenly
-      they think you sound fake. And sometimes, the ghosts' requests are just so boring…
-      So if Joss plays fast and loose with her interpretation, no one has to know, right?
-      Especially not that sucker from Spectral Self-Help who's also sniffing around
-      trying to find something to bring Ghost Whisperer down...
+      C'est un équilibre précaire: les gens veulent du drama, du drama, du drama, jusqu'au
+      moment où tout à coup ils ne vous trouvent plus crédible. Et parfois, les requêtes des
+      fantômes sont tellement ennuyeuses... Alors si Joss prend quelques libertés dans l'interprétation,
+      personne n'a besoin de le savoir, si ? Et surtout pas ce parasite qui anime Spectral Self-Help,
+      qui tourne tout le temps autour dans l'espoir de trouver quelque chose pour décrédibiliser
+      Ghost Whisperer...
     </p>
     <P>
-      Anyway, time to meet some ghosts!
+      Enfin, c'est l'heure de partir à la rencontre des fantômes !
     </p>
     <p>
-      Disclaimer: Please be aware that we do not censor the stories or vocabulary of our guests.
-      Some episodes might contain swearing. And obviously, all of them contain dead individuals.
-      We do not guarantee your ghost will move on.
+      Avertissement : Nous ne censurons ni les histoires, ni le vocabulaire de nos invités.
+      Certains épisodes peuvent contenir des jurons, tous contiennent des individus morts.
+      Nous ne garantissons pas le passage de votre fantôme dans l'au-delà.
     </p>
   `,
   start: "start",
-  continue: "continue",
+  continue: "suite",
 };
